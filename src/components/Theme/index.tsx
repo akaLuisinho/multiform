fed4152472255f86d4cc99abe1aae3120308c806
luisfelipe <luis.felipeszsv@gmail.com>
2021-10-11 11:27:11 -0300

    finishing base project

--- src/components/Theme/index.tsx
+++ src/components/Theme/index.tsx
@@ -1,12 +1,17 @@
 import { ReactNode } from 'react'
 import { Container, Area, Steps, Sidebar, Page } from './styles'
 import { Header } from '../Header'
+import { SidebarItem } from '../SidebarItem'
+import { useForm } from '../../contexts/FormContext'
 
 type Props = { 
     children: ReactNode
 }
 
 export function Theme({ children }: Props) {
+
+    const { state } = useForm()
+    
     return(
         <Container>
             <Area>
@@ -14,7 +19,27 @@ export function Theme({ children }: Props) {
 
                 <Steps>
                     <Sidebar>
-                        ...
+                        <SidebarItem 
+                            title="Pessoal"
+                            description="Se identifique"
+                            icon="profile"
+                            path="/"    
+                            active={state.currentStep === 1}
+                        />
+                        <SidebarItem 
+                            title="Profissional"
+                            description="Seu Nível"
+                            icon="book"
+                            path="/step2"
+                            active={state.currentStep === 2}
+                        />
+                        <SidebarItem 
+                            title="Contato"
+                            description="Como te achar?"
+                            icon="mail"
+                            path="/step3"
+                            active={state.currentStep === 3}
+                        />
                     </Sidebar>
                     <Page>
                         {children}
